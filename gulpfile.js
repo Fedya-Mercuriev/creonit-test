c797e17105b5a4625b7f9cf87bd4bc3213e3ff6b
Gleb Gorkoltsev <gleb.gorkoltsev@yandex.ru>
2018-11-04 18:09:22 +0700

    Files committed for the saving sake
    Due to a sudden Mac charger explosion I'm pushing
    all the necessary files to Github in order to be able to work on
    my other laptop

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const watch = require("gulp-watch");
+const sass = require('gulp-sass');
+
+
+sass.compiler = require('node-sass');
+
+  gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
+
+  gulp.task('watch', function() {
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+  });
