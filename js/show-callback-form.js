310c50311e565bf5ae955674323b227da3655518
Gleb Gorkoltsev <gleb.gorkoltsev@yandex.ru>
2018-11-18 12:56:55 +0700

    Callback forms are now properly invoked and shown

--- js/show-callback-form.js
+++ js/show-callback-form.js
@@ -1,35 +1,18 @@
 (function($) {
-	//  ПОлучаем контейнеры для названия и формы обратной связи
-	var titleContainer = $('.callback-form__title'),
-		formContainer = $('.callback-form__content');
+  var $orderPhoneCallBtn = $('.contact-info-list__link-order-phone-call-btn'),
+      $calculateBodyShape = $('.submit-btn');
 
-	var blockTitle = "<h3>Обратный звонок</h3>";
-	// Здесь содержатся все тэги, которые будут добавлены в обертку при нажатии на "заказать звонок"
-	var orderCallbackFormData = {
-		form: "<form class=\"js-callback-form\" action=\"\" method=\"post\">",
-		formLabelName: "<label class=\"js-callback-form__label\">Имя </label>",
-		formInputName: "<input class=\"js-callback-form__text-input\" type=\"text\" name=\"name\" value=\"\"></input>",
-		formLabelPhone: "<label class=\"js-callback-form__label\">Телефон </label>",
-		formInputPhone: "<input class=\"js-callback-form__text-input\" type=\"text\" name=\"phone-number\" value=\"\"></input>",
-		privacyPolicyText: "<p class=\"callback-form__privacy-policy\">Мы обещаем не использовать ваш номер телефона без вашего согласия. Мы не будем рассылать смс и звонить без разрешения.</p>",
-		submitButton: "<button type=\"submit\" class=\"callback-form__submit-button\">Отправить</button>"
-	};
+  $calculateBodyShape.click(function(event) {
+    event.preventDefault();
+    var calcBodyTypeCallback = new CallbackForm(calcBodyTypeFormParams);
+    calcBodyTypeCallback.render();
+    blockScroll();
+  });
 
-	// Эта функция будет собирать все элементы воедино
-	function assembleElements(elementsObj) {
-		var result = "";
-		for (var key in elementsObj) {
-			result += elementsObj[key];
-		}
-		return result;
-	}
-
-	var form = assembleElements(orderCallbackFormData);
-
-	// Добавляем полученные результаты в контейнеры
-	titleContainer.html(blockTitle);
-	formContainer.html(form);
-	
-	// Блокируем прокрутку страницы
-	blockScroll();
+  $orderPhoneCallBtn.click(function(event) {
+    event.preventDefault();
+    var callbackForm = new CallbackForm(orderPhoneCallForm);
+    callbackForm.render();
+    blockScroll();
+  })
 })(jQuery);
---
+++ js/variables.js
@@ -0,0 +1,37 @@
+var calcBodyTypeFormParams = {
+  textBlock: {
+    title: "<h2 class=\"js-callback-form-title\">Результат расчета веса</h2>",
+    paragraph: "<p class=\"js-callback-form-text\"> Похоже, лишний вес вам не грозит. А значит, вам прямая дорога в тренажерный зал — набирать мышечную массу и приобретать эффектные формы. Персональные тренировки помогут выстроить грамотную программу и добиться результатов быстрее. И не забывайте о растяжке после тренировок\!</p>"
+  },
+  form: {
+    radios: {
+      inputOne: "<input id=\"callback-reason-radio-one\" class=\"js-callback-form-radio-input\" name=\"callback-reason\" checked/>",
+      labelOne: "<label for=\"callback-reason-radio-one\" class=\"js-callback-form-label-btn\">Записаться на гостевой визит</label>",
+      inputTwo: "<input id=\"callback-reason-radio-two\" class=\"js-callback-form-radio-input\" name=\"callback-reason\" />",
+      labelTwo: "<label for=\"callback-reason-radio-two\" class=\"js-callback-form-label-btn\">Заказать карту</label>"
+    },
+    textInputs: {
+      formLabelName: "<label class=\"js-callback-form-text-label\">Имя </label>",
+      formInputName: "<input class=\"js-callback-form-text-input\" type=\"text\" name=\"name\" value=\"\"></input>",
+      formLabelPhone: "<label class=\"js-callback-form-text-label\">Телефон</label>",
+      formInputPhone: "<input class=\"js-callback-form-text-input\" type=\"text\" name=\"phone-number\" value=\"\"></input>",
+    },
+    privacyPolicyText: "<p class=\"js-callback-form-privacy-policy-text\">Мы обещаем не использовать ваш номер телефона без вашего согласия. Мы не будем рассылать смс и звонить без разрешения.</p>",
+    submitButton: "<button type=\"submit\" class=\"js-callback-form-submit-btn\">Отправить</button>"
+  }
+},
+orderPhoneCallForm = {
+  textBlock: {
+    title: "<h2 class=\"js-callback-form-title\">Обратный звонок</h2>"
+  },
+  form: {
+    textInputs: {
+      formLabelName: "<label class=\"js-callback-form-text-label\">Имя </label>",
+      formInputName: "<input class=\"js-callback-form-text-input\" type=\"text\" name=\"name\" value=\"\"></input>",
+      formLabelPhone: "<label class=\"js-callback-form-text-label\">Телефон</label>",
+      formInputPhone: "<input class=\"js-callback-form-text-input\" type=\"text\" name=\"phone-number\" value=\"\"></input>"
+    },
+    privacyPolicyText: "<p class=\"js-callback-form-privacy-policy-text\">Мы обещаем не использовать ваш номер телефона без вашего согласия. Мы не будем рассылать смс и звонить без разрешения.</p>",
+    submitButton: "<button type=\"submit\" class=\"js-callback-form-submit-btn\">Отправить</button>"
+  }
+};
