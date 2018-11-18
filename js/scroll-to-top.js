5212f80e38701ee3166dd833c90ffa3db681fe86
Gleb Gorkoltsev <gleb.gorkoltsev@yandex.ru>
2018-11-19 02:43:25 +0700

    Scroll to top btn done

---
+++ js/scroll-to-top.js
@@ -0,0 +1,25 @@
+(function() {
+  var scrollTopBtn = $('.scroll-to-top-button');
+  // Спрячем кнопку и покажем только тогда, когда половина страницы уже была
+  // проскроллена
+
+  function scrollTrack() {
+    var y = $(document).scrollTop();
+    if (y > 1000) {
+      scrollTopBtn.removeClass('scroll-to-top-button--hidden');
+    } else {
+      scrollTopBtn.addClass('scroll-to-top-button--hidden');
+    }
+  };
+
+  // Прикрутим обработчик кликов, который будет возвращать ползователя на верх
+  // страницы, если тот кликнет по кнопке "вверх"
+  scrollTopBtn.click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    return false;
+  })
+
+  $(document).scroll(scrollTrack);
+  // scroll();
+})();
