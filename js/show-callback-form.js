18157a5784d4e08f3d46f838d0548db8680c533e
Gleb Gorkoltsev <gleb.gorkoltsev@yandex.ru>
2018-11-18 20:37:04 +0700

    Fixed custom radio-buttons in callback form
    Custom radio buttons now work properly and display their correct state
    (checked or not checked).

--- js/show-callback-form.js
+++ js/show-callback-form.js
@@ -2,11 +2,28 @@
   var $orderPhoneCallBtn = $('.contact-info-list__link-order-phone-call-btn'),
       $calculateBodyShape = $('.submit-btn');
 
+      function styleCallbackRadios() {
+        var $inputsContainer = $('.js-callback-form-radios-wrapper'),
+            $buttons = $inputsContainer.contents().filter('label');
+
+        $inputsContainer.on('click', 'label', function(event) {
+          // event.stopPropagation();
+          $(event.target).addClass('js-callback-form-label-btn--is-checked');
+          $.each($buttons, function(key) {
+            if ($buttons[key] != event.target) {
+              $buttons[key].classList.remove('js-callback-form-label-btn--is-checked');
+            }
+          });
+        })
+      }
+
+
   $calculateBodyShape.click(function(event) {
     event.preventDefault();
     var calcBodyTypeCallback = new CallbackForm(calcBodyTypeFormParams);
     calcBodyTypeCallback.render();
     blockScroll();
+    styleCallbackRadios();
   });
 
   $orderPhoneCallBtn.click(function(event) {
