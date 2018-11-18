5dd61f6bca6a9e6c19bb531103366aeef7f944fe
Gleb Gorkoltsev <gleb.gorkoltsev@yandex.ru>
2018-11-18 22:52:31 +0700

    Fixed

--- js/close-callback.js
+++ js/close-callback.js
@@ -3,15 +3,28 @@
 			overlay = $('.js-callback-form-overlay'),
 			formWrapper = $('.js-callback-form-wrapper');
 
+	function cleaFormContent() {
+		var formContent = $('.js-callback-form-content');
+		formContent.empty();
+	}
+
+// Удаляем форму при нажатии на крестик фон
 	overlay.on('click', ".close-form-btn", function(event) {
-		overlay.hide();
-		formWrapper.hide();
+		// Очищаем форму
+		cleaFormContent()
+		// Прячем ее
+		overlay.removeClass('js-callback-form-overlay--shown');
+		formWrapper.addClass('js-callback-form-wrapper--hidden');
 		event.stopPropagation();
 	});
 
+// Удаляем форму при нажатии на затемненный фон
 	overlay.click('click', function(event) {
-		overlay.hide();
-		formWrapper.hide();
+		// Очищаем форму
+		cleaFormContent()
+		// Прячем ее
+		overlay.removeClass('js-callback-form-overlay--shown');
+		formWrapper.addClass('js-callback-form-wrapper--hidden');
 		event.stopPropagation();
 	})
 }
